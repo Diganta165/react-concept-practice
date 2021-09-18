1ca6d0bcd7f4f033f9e321aa6a756c1bc2e7a9f9
Diganta165 <mdsrdiganta@gmail.com>
2021-09-18 11:23:23 +0600

    Dynamically passed props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const cinemas = [
+  {nayok: 'Koober', Nayika: 'Kopila'},
+  {nayok: 'Rubel', Nayika: 'Shabana'},
+  {nayok: 'Ananta Jalil', Nayika: 'Borsha'}
+]
 function App() {
   return (
     <div className="App">
-      <Person name = 'Sakib Al Hasan' job = 'Cricketer' ></Person>
-      <Person name = 'l3n' job = 'Pro Gamer' ></Person>
+      {/* <Person name = 'Sakib Al Hasan' job = 'Cricketer' ></Person>
+      <Person name = 'l3n' job = 'Pro Gamer' ></Person> */}
+
+      {
+        cinemas.map(cinema => <Cinema nayok = {cinema.nayok} nayika ={cinema.Nayika}></Cinema>)
+      }
      
     </div>
   );
@@ -28,6 +38,21 @@ function Person(props){
       <h1 >Name: {props.name}</h1>
       <p>Job: {props.job}</p>
 
+    </div>
+  );
+}
+
+function Cinema(props){
+  const stylePlayer = {
+    backgroundColor: 'skyblue',
+    border: '3px solid lightsalmon',
+    margin: '20px',
+    borderRadius: '10px'
+  }
+  return(
+    <div style={stylePlayer}>
+      <h1>Nayok: {props.nayok}</h1>
+      <h1>Nayika: {props.nayika}</h1>
     </div>
   );
 }
