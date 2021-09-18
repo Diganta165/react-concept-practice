0f4aa3e5fa0fd2c142e73bdc8a7b80c7722e5909
Diganta165 <mdsrdiganta@gmail.com>
2021-09-18 13:37:06 +0600

    Loaded user data by fetching API & used useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 const cinemas = [
@@ -19,6 +19,7 @@ function App() {
       }
      
      <Count></Count>
+     <ExternalUsers></ExternalUsers>
 
     </div>
   );
@@ -79,6 +80,36 @@ function Count (){
       <button onClick ={handleDecrease}>Decrease</button>
     </div>
 
+  );
+}
+
+// Load User with API 
+
+function ExternalUsers (){
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=> res.json())
+    .then(data => setUsers(data))
+  },[]);
+
+  return(
+    <div>
+      <h3>External Users</h3>
+      {
+        users.map(user=> <User name = {user.name} email= {user.email}></User>)
+      }
+    </div>
+  );
+}
+
+function User(props){
+  return(
+    <div>
+      <h4>{props.name}</h4>
+      <p>{props.email}</p>
+    </div>
   );
 }
 
